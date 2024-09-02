f3c19da1895bebc0c8e069b8e97b532cbc95b22d
Bhavna Jain <jbhavna732@gmail.com>
2024-09-03 01:45:19 +0530

    useRef Hook and form validation

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # Netflix-GPT
 - Create React App
 - Configured TailwindCss
+- Header
+- Login Form
+
+
+# Features
+- Login/signup Screen
+  - Sign In/Sign Up Form
+  - Redirect to Browse Page
+
+- Browse (after authentications)
+  - Header
+  - Main Movie
+    - Trailer in background
+    - Movie title and description
+    - Movie Suggestions
+     - Movielists * N 
+- Netflix Gpt
+  - Search Bar
+  - Movie Suggestions
+
+
--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Header from './Header';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
+//import { checkValidData } from '../utils/validate';
 
 const Login = () => {
 const [isSignInForm, setIsSignInForm] = useState(true);
+const email = useRef(null);
+const password = useRef(null);
+
  const handleButtonClick = () => {
+  // validate the form data
+  //checkValidData(email, password);
+  console.log(email.current.value);
+  console.log(password.current.value);
  }
  const toggleSignInForm = () => {
     setIsSignInForm(!isSignInForm)
@@ -13,15 +21,15 @@ const [isSignInForm, setIsSignInForm] = useState(true);
     <div >
         <Header />
         <div className='absolute'>
-            <img src='https://assets.nflxext.com/ffe/siteui/vlv3/dae1f45f-c2c5-4a62-8d58-6e1b0c6b2d8e/6d1fb8a4-5844-42a4-9b01-1c6c128acf19/IN-en-20240827-TRIFECTA-perspective_WEB_c292a608-cdc6-4686-8dc8-405bfcf753af_small.jpg' alt='Background Image'/>
+            <img src='https://assets.nflxext.com/ffe/siteui/vlv3/dae1f45f-c2c5-4a62-8d58-6e1b0c6b2d8e/6d1fb8a4-5844-42a4-9b01-1c6c128acf19/IN-en-20240827-TRIFECTA-perspective_WEB_c292a608-cdc6-4686-8dc8-405bfcf753af_small.jpg' alt='background'/>
         </div>
-        <form className='w-3/12 absolute p-12 bg-black  bg-blend-soft-light bg-opacity-80 rounded my-36 mx-auto right-0 left-0'>
+        <form onSubmit={(e) => e.preventDefault()} className='w-3/12 absolute p-12 bg-black  bg-blend-soft-light bg-opacity-80 rounded my-36 mx-auto right-0 left-0'>
             <h1 className='text-3xl font-bold py-4 text-white'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
             {!isSignInForm && <input type='text' placeholder='Full Name' className='w-full px-4 py-3 my-4 bg-zinc-900 border border-white rounded text-white'/>}
-            <input type='email' placeholder='Email' className='w-full px-4 py-3 my-4 bg-zinc-900 border border-white rounded text-white'/>
+            <input ref={email} type='email' placeholder='Email' className='w-full px-4 py-3 my-4 bg-zinc-900 border border-white rounded text-white'/>
             
-            <input type='password' placeholder='Password' className='w-full px-4 py-3 my-4 bg-zinc-900 border border-white rounded text-white'/>
-            <button onSubmit={(e) => e.preventDefault()} className='w-full px-4 py-3 my-6 bg-red-600 text-white rounded' onClick={handleButtonClick}>{isSignInForm ? "Sign In" : "Sign Up"} </button>
+            <input ref={password} type='password' placeholder='Password' className='w-full px-4 py-3 my-4 bg-zinc-900 border border-white rounded text-white'/>
+            <button  className='w-full px-4 py-3 my-6 bg-red-600 text-white rounded' onClick={handleButtonClick}>{isSignInForm ? "Sign In" : "Sign Up"} </button>
             <h3 className='text-white'>Forgot Password?</h3>
             <p className='text-white py-4 cursor-pointer' onClick={toggleSignInForm}>{isSignInForm ? "New to Netflix? Sign Up Now" : "Already Registered? Sign In Now"}</p>
         </form>
