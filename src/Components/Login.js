22778c415a11b2b3343b91ca47ac62b5cf9103d6
Bhavna Jain <jbhavna732@gmail.com>
2024-09-23 15:34:25 +0530

    createauth and signinauth

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Header from './Header';
 import { useState, useRef } from 'react';
 import { checkValidData } from '../utils/validate';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import {auth} from "../utils/firebase";
 
 const Login = () => {
 const [isSignInForm, setIsSignInForm] = useState(true);
@@ -12,17 +14,59 @@ const password = useRef(null);
 
  const handleButtonClick = () => {
   // validate the form data
-  if(!name == null){
-    let message = checkValidData(email.current.value, password.current.value,name.current.value);
+  if(name == null){
+    var message = checkValidData(email.current.value, password.current.value,name.current.value);
 
     setErrorMessage(message);
     console.log(message)
   }
   else{
-    let message = checkValidData(email.current.value,password.current.value);
+    var  message = checkValidData(email.current.value,password.current.value);
     setErrorMessage(message)
   }
   
+  if(message) return;
+
+    // sign in / sign up
+
+  if(!isSignInForm){
+      // SignUp Logic
+      createUserWithEmailAndPassword(auth, 
+        email.current.value, 
+        password.current.value
+      )
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    console.log(user)
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    setErrorMessage(errorCode + "-" + errorMessage)
+    // ..
+  });
+   }
+   else{
+     // Sign In Logic
+     signInWithEmailAndPassword(auth, 
+      email.current.value,
+       password.current.value)
+     .then((userCredential) => {
+       // Signed in 
+       const user = userCredential.user;
+       console.log(user)
+       // ...
+     })
+     .catch((error) => {
+       const errorCode = error.code;
+       const errorMessage = error.message;
+       setErrorMessage(errorCode + "-" + errorMessage)
+     });
+
+    
+   }
   
   // console.log(email.current.value);
   // console.log(password.current.value);
@@ -39,12 +83,12 @@ const password = useRef(null);
         </div>
         <form onSubmit={(e) => e.preventDefault()} className='w-3/12 absolute p-12 bg-black  bg-blend-soft-light bg-opacity-80 rounded my-36 mx-auto right-0 left-0'>
             <h1 className='text-3xl font-bold py-4 text-white'>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
-            {!isSignInForm && <input ref= {name} type='text' placeholder='Full Name' className='w-full px-4 py-3 my-4 bg-zinc-900 border border-white rounded text-white'/>}
+            {!isSignInForm && <input ref={name} type='text' placeholder='Full Name' className='w-full px-4 py-3 my-4 bg-zinc-900 border border-white rounded text-white'/>}
             <input ref={email} type='email' placeholder='Email' className='w-full px-4 py-3 my-4 bg-zinc-900 border border-white rounded text-white'/>
             {/* <p className='text-red-600 '>{errorMessage}</p> */}
             <input ref={password} type='password' placeholder='Password' className='w-full px-4 py-3 my-4 bg-zinc-900 border border-white rounded text-white'/>
             <p className='text-red-600 font-bold py-2'>{errorMessage}</p>
-            <button  className='w-full px-4 py-3 my-6 bg-red-600 text-white rounded' onClick={handleButtonClick}>{isSignInForm ? "Sign In" : "Sign Up"} </button>
+            <button   className='w-full px-4 py-3 my-6 bg-red-600 text-white rounded' onClick={handleButtonClick}>{isSignInForm ? "Sign In" : "Sign Up"} </button>
             <h3 className='text-white'>Forgot Password?</h3>
             <p className='text-white py-4 cursor-pointer' onClick={toggleSignInForm}>{isSignInForm ? "New to Netflix? Sign Up Now" : "Already Registered? Sign In Now"}</p>
         </form>
--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,21 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAzecStoucNyX82div3sqeKVv5QO2RKytI",
-  authDomain: "netflixgpt-65ce0.firebaseapp.com",
-  projectId: "netflixgpt-65ce0",
-  storageBucket: "netflixgpt-65ce0.appspot.com",
-  messagingSenderId: "253418281268",
-  appId: "1:253418281268:web:004895589a5de1bebeed61",
-  measurementId: "G-M67291E9K8"
+  apiKey: "AIzaSyBzJ3QqH-2PZ4EPSmnM-_EzsyoarAofxwI",
+  authDomain: "netflixgpt-e3a40.firebaseapp.com",
+  projectId: "netflixgpt-e3a40",
+  storageBucket: "netflixgpt-e3a40.appspot.com",
+  messagingSenderId: "377242303346",
+  appId: "1:377242303346:web:942939e821da4d52827beb",
+  measurementId: "G-89QVPD7KJ9"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const auth = getAuth();
